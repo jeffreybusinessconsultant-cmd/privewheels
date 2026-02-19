53bf3d2673228633b6db41f4e5776c6dfc46a912
Sibiyoan <sibiyoanyoans@gmail.com>
2026-02-19 11:41:22 +0530

    Remove Replit branding and logo from website

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,6 @@ export default defineConfig(async () => {
     runtimeErrorOverlay(),
   ];
 
-  if (process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined) {
-    const cartographer = await import("@replit/vite-plugin-cartographer").then((m) => m.cartographer());
-    const devBanner = await import("@replit/vite-plugin-dev-banner").then((m) => m.devBanner());
-    plugins.push(cartographer, devBanner);
-  }
-
   return {
   plugins,
   resolve: {
